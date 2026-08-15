2861b5e3d01ac371b3d8da66ab84880263bc170e
SCENTIAMX <scentia24@gmail.com>
2026-08-15 07:17:23 -0600

    Fix formatting in correo.js email sender field

--- api/correo.js
+++ api/correo.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json'
       }, 
       body: JSON.stringify({
-        from: 'SCENTIA <hola@scentia.mx>',
+        from: 'SCENTIA <hola@scentia.mx>', 
         to: email,
         subject: `🖤 Tu acceso exclusivo a SCENTIA, ${nombre}!`,
         html: htmlContent
